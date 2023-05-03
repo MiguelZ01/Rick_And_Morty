f6859b8ee159f3e7c981fcd527185ef9cb338ed1
MiguelZ01 <jesusmigu3lzr2004@gmail.com>
2023-05-03 17:32:39 -0500

    Archivos subidos

--- Server/src/models/User.js
+++ Server/src/models/User.js
@@ -1,5 +1,26 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-   sequelize.define('User', {}, { timestamps: false });
+  sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: false,
+      },
+
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        isEmail: true,
+      },
+
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
 };
